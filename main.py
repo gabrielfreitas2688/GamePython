55955efdb8a2e0ea2baf6b3402a7b4b6492570d9
Gabriel Freitas <gabrielfreitas2688@gmail.com>
2023-03-21 11:58:16 -0300

    Criação inicial do plano background da plataforma e inserção dos itens de front na pasta

---
+++ main.py
@@ -0,0 +1,61 @@
+import os 
+import random
+import math
+import pygame
+from os import listdir
+from os.path import isfile, join
+pygame.init()
+
+pygame.display.set_caption("Plataforma")
+
+WIDHT, HEIGHT = 1000, 800
+FPS = 60
+PLAYER_VEL = 5
+
+window = pygame.display.set_mode((WIDHT, HEIGHT))
+
+#Pega os backgrounds da tela da pasta do diretório
+def get_background(name):
+    image = pygame.image.load(join("assets", "Background", name))
+    __, __, width, height = image.get_rect()
+    tiles = []
+
+#Preenche cada background nos pixels pré definidos da tela
+    for i in range(WIDHT // width + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = [i * width, j * height]
+            tiles.append(pos)
+    
+    return tiles, image
+
+def draw(window, background, bg_image):
+    for tile in background:
+        window.blit(bg_image, tile)
+
+        pygame.display.update()
+
+
+def main(window):
+    clock = pygame.time.Clock()
+    background, bg_image = get_background("Pink.png")
+
+# Fixa o valor de FPS máximo em 60 
+    run = True
+    while run:
+        clock.tick(FPS)
+        
+# Função QUIT pra fechar o jogo
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+        draw(window, background, bg_image)
+
+    pygame.quit()
+    quit()        
+
+
+if __name__ == "__main__":
+    main(window)
+
